23cd114a2dcfe23e99523cfa8c87f11524e36d6e
Jojo Mukeke <jmills605@gmail.com>
2023-03-18 11:19:25 +0000

    new code added for working generator

---
+++ script.js
@@ -0,0 +1,63 @@
+// Array of special characters to be included in password
+
+
+var specialCharacters = 8;
+var choiceArray = [];
+
+var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '|', '~'];
+var lowerCasedCharacters =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',]; 
+var upperCasedCharacters =['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
+var numericCharacters =['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+
+// Assignment code here
+var generateBtn = document.querySelector("#generate");
+
+//Add Event Listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Write password to the #password input
+function writePassword() {
+  var correctPrompts = getPrompts();
+  var passwordText = document.querySelector("#password");
+  if (correctPrompts) {
+  var newPassword = generatePassword();
+  passwordText.value = newPassword;
+  } else {
+  passwordText.value = "";
+  }
+}
+
+function generatePassword() {
+  var password = "";
+  for (var i = 0; i < characterLength; i++) {
+    var randomIndex = Math.floor(Math.random() * choiceArray.length)
+    password = password + choiceArray[randomIndex];
+  }
+  return password;
+}
+
+function getPrompts() {
+  choiceArray = [];
+  characterLength = parseInt(prompt("How many characters do you want your password to be? (8-128 Characters)"));
+
+  if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    alert("Character length invalid: type an integer between 8-128. Please try again!");
+    return false;
+  }
+  if (confirm("Would you like lowercase letters in your password? (OK = YES, Cancel = NO)")) {
+      choiceArray = choiceArray.concat(lowerCasedCharacters);
+    }
+
+  if (confirm("Would you like uppercase letters in your password? (OK = YES, Cancel = NO)")) {
+      choiceArray = choiceArray.concat(upperCasedCharacters);
+    }
+
+  if (confirm("Would you like numbers in your password? (OK = YES, Cancel = NO)")) {
+      choiceArray = choiceArray.concat(numericCharacters); 
+    }
+
+  if (confirm("Would you like special characters in your password? (OK = YES, Cancel = NO)")) {
+      choiceArray = choiceArray.concat(specialCharacters);
+    }
+    return true;
+}
